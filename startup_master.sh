a8a7a71718f664eac10a20dc3c42b46ed96bb7e1
Anwesha-p <anwesha.pradhananga@gmail.com>
2024-07-02 14:30:57 -0400

    added file changes

--- startup_master.sh
+++ startup_master.sh
@@ -134,11 +134,8 @@ if ! sudo python3 -c "import salt.client" &> /dev/null; then
     echo "Salt Python client is not installed. Installing now..."
     sudo apt-get install -y python3-pip python3-m2crypto python3-zmq
     sudo rm /usr/lib/python3.*/EXTERNALLY-MANAGED
-    pwd
-    cd /home/
-    pwd
-    ls
-    #pip3 install salt
+    cd /home/ubuntu
+    /usr/bin/python3 -m pip install salt
 fi
 
     
