db2ea44e34ba0e8947f8ceea768cc0d34d48de5a
Anwesha-p <anwesha.pradhananga@gmail.com>
2024-07-05 09:31:23 -0400

    added venv

--- startup_master.sh
+++ startup_master.sh
@@ -141,8 +141,8 @@ if ! sudo python3 -c "import salt.client" &> /dev/null; then
     echo "Salt Python client is not installed. Installing now..."
     sudo apt-get install -y python3-pip python3-m2crypto python3-zmq
     sudo rm /usr/lib/python3.*/EXTERNALLY-MANAGED
-    sudo apt-get install python3-venv
-    python3 -m venv myenv
+    sudo apt install python3.12-venv
+    python3 -m venv /home/ubuntu/myenv
     source myenv/bin/activate
     python3 -m pip install salt
     echo "Salt Python client installed for user."
